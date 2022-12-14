f5a3ef2bb418117873132fa5d71a6ca95714ab9b
Roman Tarnakin <roman_tarnakin@epam.com>
2022-12-14 20:25:26 +0300

    try styled-components and css modules approaches for styling

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,44 @@
 import React, { useState } from "react";
 
 import Button from "../../UI/Button/Button";
-import "./CourseInput.css";
+import styles from "./CourseInput.module.css";
+// import styled from "styled-components";
+
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
+//
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color: ${(props) => (props.invalid ? "red" : "black")};
+//   }
+//
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${(props) => (props.invalid ? "#d0aeae" : "#ccc")};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//     background-color: ${(props) => (props.invalid ? "#d0aeae" : "transparent")};
+//   }
+//
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
+//
+//   //&.invalid label {
+//   //  color: red;
+//   //}
+//
+//   //&.invalid input {
+//   //  background-color: #d0aeae;
+//   //  border-color: red;
+//   //}
+// `;
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -30,7 +67,16 @@ const CourseInput = (props) => {
   return (
     <form onSubmit={formSubmitHandler}>
       {/*another approach is to conditionally set className value with template literals and conditions*/}
-      <div className={`form-control ${!isValid ? "invalid" : ""}`}>
+      {/*<FormControl className={`form-control ${!isValid ? "invalid" : ""}`}>*/}
+      {/*when using styled-components we basically use props and work with them inside styled component to
+       dynamically evaluate styles*/}
+      {/*<FormControl invalid={!isValid}>*/}
+      <div
+        className={`${styles["form-control"]} ${
+          !isValid ? styles.invalid : ""
+        }`}
+      >
+        {/*<div className={styles[`form-control ${!isValid ? "invalid" : ""}`]}>*/}
         {/*one way of conditional styles application is using inline style with some conditions*/}
         {/*<label style={{ color: isValid ? "black" : "red" }}>Course Goal</label>*/}
         <label>Course Goal</label>
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,10 +1,39 @@
-import React from "react";
+// import styled from "styled-components";
+// let's try CSS Modules approach to support separation of concerns in terms of styles being written inside css-files
+// 1) we need to rename the file in a special way - add 'module'
+// 2) we need to import an object (the object name is up to us) from this file
+import styles from "./Button.module.css";
 
-import "./Button.css";
+// let's try styled-components approach for styling purposes
+// const Button = styled.button`
+//   width: 100%;
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
+//
+//   @media (min-width: 768px) {
+//     width: auto;
+//   }
+//
+//   &:focus {
+//     outline: none;
+//   }
+//
+//   &:hover,
+//   &:active {
+//     background: #ac0e77;
+//     border-color: #ac0e77;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
+// `;
 
 const Button = (props) => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
