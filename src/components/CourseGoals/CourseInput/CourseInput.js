6f16035b937e31d3d49f5131e0a97e923f96d2c7
Roman Tarnakin <roman_tarnakin@epam.com>
2022-12-14 16:23:35 +0300

    try inline styles and template literals classname approaches for conditional styling

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,25 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
 
-  const goalInputChangeHandler = event => {
+  const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
+
     setEnteredValue(event.target.value);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
+
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+
     props.onAddGoal(enteredValue);
+    setEnteredValue("");
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      {/*another approach is to conditionally set className value with template literals and conditions*/}
+      <div className={`form-control ${!isValid ? "invalid" : ""}`}>
+        {/*one way of conditional styles application is using inline style with some conditions*/}
+        {/*<label style={{ color: isValid ? "black" : "red" }}>Course Goal</label>*/}
         <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <input
+          type="text"
+          onChange={goalInputChangeHandler}
+          value={enteredValue}
+          // style={{
+          //   border: isValid ? "1px solid black" : "1px solid red",
+          //   backgroundColor: isValid ? "transparent" : "salmon",
+          // }}
+        />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
